15780aada519209b8d50dc17fb128356e5b7d573
Brady Heath <brheath@iastate.edu>
2022-02-05 22:45:51 -0600

    testing eclipse with comments

--- Experiments/BradyHeath/exp1/src/main/java/com/example/demo/Exp1Application.java
+++ Experiments/BradyHeath/exp1/src/main/java/com/example/demo/Exp1Application.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Exp1Application {
 
+	//testing comments then pushing
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Exp1Application.class, args);
 	}
