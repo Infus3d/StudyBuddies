00efb1064c70785996dbebde3d17ef051600eb10
Omar Muhammetkulyyev <omar99@iastate.edu>
2022-02-07 09:11:12 -0600

    Random Color Selection

--- Experiments/OmarMuhammetkulyyev/Experiment1/app/src/main/java/com/example/hello_world/SecondFragment.java
+++ Experiments/OmarMuhammetkulyyev/Experiment1/app/src/main/java/com/example/hello_world/SecondFragment.java
@@ -1,17 +1,20 @@
 package com.example.hello_world;
 
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import androidx.annotation.ColorInt;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.hello_world.databinding.FragmentSecondBinding;
 
+import java.util.Locale;
 import java.util.Random;
 
 public class SecondFragment extends Fragment {
@@ -31,17 +34,21 @@ public class SecondFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        Integer count = SecondFragmentArgs.fromBundle(getArguments()).getMyArg();
-        String countText = getString(R.string.random_heading, count);
+        //Integer count = SecondFragmentArgs.fromBundle(getArguments()).getMyArg();
+        String hex_color = SecondFragmentArgs.fromBundle(getArguments()).getMyArg();
+        String countText = getString(R.string.random_heading, hex_color);
         TextView headerView = view.getRootView().findViewById(R.id.textview_header);
+        View myRootView = view.getRootView();
+        myRootView.setBackgroundColor(get_Int(hex_color));
+        myRootView.setBackground(new ColorDrawable(get_Int(hex_color.toLowerCase())));
         headerView.setText(countText);
-        Random random = new java.util.Random();
-        Integer randomNumber = 0;
-        if (count > 0) {
-            randomNumber = random.nextInt(count + 1);
-        }
-        TextView randomView = view.getRootView().findViewById(R.id.textView_random);
-        randomView.setText(randomNumber.toString());
+//        Random random = new java.util.Random();
+//        Integer randomNumber = 0;
+//        if (count > 0) {
+//            randomNumber = random.nextInt(count + 1);
+//        }
+//        TextView randomView = view.getRootView().findViewById(R.id.textView_random);
+//        randomView.setText(randomNumber.toString());
 
         binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,6 +57,11 @@ public class SecondFragment extends Fragment {
                         .navigate(R.id.action_SecondFragment_to_FirstFragment);
             }
         });
+    }
+    @ColorInt
+    private int get_Int(String hex_color){
+        int ret = Integer.parseInt(hex_color, 16);
+        return ret;
     }
 
     @Override
