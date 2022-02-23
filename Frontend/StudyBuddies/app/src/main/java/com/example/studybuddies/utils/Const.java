94609555516fa2e0b5d1f832f82c2f9891230450
Andy Bruder <ajbruder@iastate.edu>
2022-02-22 19:49:56 -0600

    some edits to project structure and added a utils folder for urls

---
+++ Frontend/StudyBuddies/app/src/main/java/com/example/studybuddies/utils/Const.java
@@ -0,0 +1,5 @@
+package com.example.studybuddies.utils;
+
+public class Const {
+    public static final String URL_JSON_OBJECT = "https://24e94098-8e0a-43ef-a02f-c6cb85bcfab1.mock.pstmn.io/user";
+}
