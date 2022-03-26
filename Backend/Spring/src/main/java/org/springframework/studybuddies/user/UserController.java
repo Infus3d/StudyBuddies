96246fb5e2bf0ba0d91ff1e597a53ef138c5ebea
Ryan <Ryan Sand>
2022-03-26 14:33:45 -0500

    Cleaned up user baseline project

--- Backend/Spring/src/main/java/org/springframework/studybuddies/user/UserController.java
+++ Backend/Spring/src/main/java/org/springframework/studybuddies/user/UserController.java
@@ -1,19 +1,5 @@
-/*
- * Copyright 2002-2013 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package org.springframework.samples.users.user;
+
+package org.springframework.studybuddies.user;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,20 +11,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-//import coms309.people.Person;
-
 import java.util.List;
-//import java.util.Optional;
+
 
 /**
- * @author Juergen Hoeller
- * @author Ken Krebs
- * @author Arjen Poutsma
- * @author Michael Isvy
- * @Modified By Tanmay Ghosh
- * @Modified By Vivek Bengre
+ * @author Ryan Sand and Brady Heath
  */
+
+
 @RestController
 class UserController {
 
---
+++ Backend/Spring/src/main/java/org/springframework/studybuddies/user/Users.java
@@ -0,0 +1,82 @@
+package org.springframework.studybuddies.user;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.NotFound;
+import org.hibernate.annotations.NotFoundAction;
+import org.springframework.core.style.ToStringCreator;
+
+/**
+ * @author Ryan Sand and Brady Heath
+ */
+
+
+@Entity
+@Table(name = "users")
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    @NotFound(action = NotFoundAction.IGNORE)
+    private Integer id;
+
+    @Column(name = "first_name")
+    @NotFound(action = NotFoundAction.IGNORE)
+    private String firstName;
+
+    @Column(name = "last_name")
+    @NotFound(action = NotFoundAction.IGNORE)
+    private String lastName;
+
+    public Users(){
+        
+    }
+
+    public Users(int id, String firstName, String lastName){
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+       
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
+    @Override
+    public String toString() {
+        return new ToStringCreator(this)
+
+                .append("id", this.getId())
+                .append("lastName", this.getLastName())
+                .append("firstName", this.getFirstName())
+               .toString();
+    }
+}
