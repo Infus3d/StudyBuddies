b47ed830986f55a95f885a370503175b4d44b661
Ryan <Ryan Sand>
2022-03-28 19:06:05 -0500

    Added CRUDL to groups

--- Backend/Spring/src/main/java/org/springframework/studybuddies/group/GroupController.java
+++ Backend/Spring/src/main/java/org/springframework/studybuddies/group/GroupController.java
@@ -6,8 +6,10 @@ import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -27,9 +29,11 @@ public class GroupController {
 
     private final Logger logger = LoggerFactory.getLogger(GroupController.class);
     
+    //CRUDL
+    
     //CREATE Request
     @PostMapping("/groups/new")
-    public @ResponseBody String createUsers(@RequestBody Groups group) {
+    public @ResponseBody String createGroups(@RequestBody Groups group) {
         System.out.println(group);
         groupsRepository.save(group);
         return "New Group "+ group.getTitle() + " Saved";
@@ -43,6 +47,34 @@ public class GroupController {
         return results;
     }
     
+    
+    //UPDATE Request
+    @PutMapping("/groups/{groupId}")
+    public @ResponseBody String updateGroups(@RequestBody Groups request,
+    		@PathVariable("groupId") int id) {
+        Optional<Groups> group = groupsRepository.findById(id);
+    	if(group == null) return null;
+    	
+    	group.get().setTitle(request.getTitle());
+    	group.get().setIsPublic(request.getIsPublic());
+    	
+        System.out.println(group.get());
+        groupsRepository.save(group.get());
+        return "Group "+ group.get().getTitle() + " Saved";
+    }
+    
+    //DELETE Request
+    @DeleteMapping("/groups/{groupId}")
+    public String deleteGroupById(@PathVariable("groupId") int id) {
+        logger.info("Entered into Controller Layer");
+        Optional<Groups> group = groupsRepository.findById(id);
+        if(group == null) return null;
+        
+        groupsRepository.deleteById(id);
+        
+        return "Group " + group.get().getTitle() + " Deleted";
+    }
+    
     //LIST Request
     @RequestMapping(method = RequestMethod.GET, path = "/groups")
     public List<Groups> getAllGroups() {
@@ -51,5 +83,8 @@ public class GroupController {
         logger.info("Number of Records Fetched:" + results.size());
         return results;
     }
+    
+    
+    
 	
 }
