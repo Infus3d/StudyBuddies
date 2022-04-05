45d8cbaa94f7d2f7c7b5e7501d68c78df28749fc
Andy Bruder <ajbruder@iastate.edu>
2022-04-04 21:49:25 -0500

    helper class and dashboard changes

--- Frontend/StudyBuddies/app/src/main/java/com/example/studybuddies/utils/PopulateUserGroups.java
+++ Frontend/StudyBuddies/app/src/main/java/com/example/studybuddies/utils/PopulateUserGroups.java
@@ -1,2 +1,13 @@
-package com.example.studybuddies.utils;public class PopulateUserGroups {
+package com.example.studybuddies.utils;
+
+import org.json.JSONArray;
+
+public class PopulateUserGroups {
+
+    public static JSONArray getGroups() {
+
+        return null;
+
+    }
+
 }
