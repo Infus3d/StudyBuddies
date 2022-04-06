18934732c2a7182276add4dda27200de89d85d66
Andy <ajbruder@iastate.edu>
2022-04-05 23:18:01 -0500

    created interfaces whose methods will be called whenever someone calls a RequestsCentral method. these will help pass the response out of the RequestsCentral class and allow action to be taken on the response outside of the request

---
+++ Frontend/StudyBuddies/app/src/main/java/com/example/studybuddies/utils/OnSuccessfulArray.java
@@ -0,0 +1,10 @@
+package com.example.studybuddies.utils;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public interface OnSuccessfulArray {
+
+    public void onSuccess(JSONArray response);
+
+}
---
+++ Frontend/StudyBuddies/app/src/main/java/com/example/studybuddies/utils/OnSuccessfulObject.java
@@ -0,0 +1,8 @@
+package com.example.studybuddies.utils;
+
+import org.json.JSONObject;
+
+public interface OnSuccessfulObject {
+
+    public void onSuccess(JSONObject response);
+}
