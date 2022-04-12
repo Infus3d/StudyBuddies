cb24499cd578450efa26ebd1b9c674fe94a93689
Andy <ajbruder@iastate.edu>
2022-04-11 22:28:25 -0500

    more javadoc again

--- Frontend/StudyBuddies/app/src/main/java/com/example/studybuddies/app/AppController.java
+++ Frontend/StudyBuddies/app/src/main/java/com/example/studybuddies/app/AppController.java
@@ -10,10 +10,13 @@ import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.Volley;
 
 /**
- * This class is used to handle request qeues
+ * This class is used to handle request queues
  */
 public class AppController extends Application {
 
+	/**
+	 * Tag for this class
+	 */
 	public static final String TAG = AppController.class.getSimpleName();
 
 	private RequestQueue mRequestQueue;
@@ -27,10 +30,19 @@ public class AppController extends Application {
 		mInstance = this;
 	}
 
+
+	/**
+	 * returns the instance of the AppController
+	 * @return
+	 */
 	public static synchronized AppController getInstance() {
 		return mInstance;
 	}
 
+	/**
+	 * Creates a new request queue if null, else return the request queue
+	 * @return
+	 */
 	public RequestQueue getRequestQueue() {
 		if (mRequestQueue == null) {
 			mRequestQueue = Volley.newRequestQueue(getApplicationContext());
@@ -39,6 +51,7 @@ public class AppController extends Application {
 		return mRequestQueue;
 	}
 
+	/*
 	public ImageLoader getImageLoader() {
 		getRequestQueue();
 		if (mImageLoader == null) {
@@ -47,18 +60,34 @@ public class AppController extends Application {
 		}
 		return this.mImageLoader;
 	}
+	 */
 
+	/**
+	 * Adds a new Volley Request to the queue
+	 * @param req the request to be added to the queue
+	 * @param tag the tag to go along with the request
+	 * @param <T>
+	 */
 	public <T> void addToRequestQueue(Request<T> req, String tag) {
 		// set the default tag if tag is empty
 		req.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
 		getRequestQueue().add(req);
 	}
 
+	/**
+	 * Adds a new Volley Request to the queue
+	 * @param req the request to be added to the queue
+	 * @param <T>
+	 */
 	public <T> void addToRequestQueue(Request<T> req) {
 		req.setTag(TAG);
 		getRequestQueue().add(req);
 	}
 
+	/**
+	 * Cancels all pending requests with the given tag
+	 * @param tag tag to indicate which requests to cancel
+	 */
 	public void cancelPendingRequests(Object tag) {
 		if (mRequestQueue != null) {
 			mRequestQueue.cancelAll(tag);
--- Frontend/StudyBuddies/app/src/main/java/com/example/studybuddies/utils/Const.java
+++ Frontend/StudyBuddies/app/src/main/java/com/example/studybuddies/utils/Const.java
@@ -4,10 +4,28 @@ package com.example.studybuddies.utils;
  * This class contains the URLs for requests
  */
 public class Const {
+    /**
+     * Url for the GET function which will return all users
+     */
     public static final String GET_USERS = "http://coms-309-011.class.las.iastate.edu:8080/users";
+    /**
+     * Url for the POST function which will create a new user
+     */
     public static final String CREATE_NEW_USER = "http://coms-309-011.class.las.iastate.edu:8080/users/new";
+    /**
+     * Url for the GET function which will return all groups
+     */
     public static final String GET_GROUPS = "http://coms-309-011.class.las.iastate.edu:8080/groups";
+    /**
+     * Url for the POST function which will create a new group
+     */
     public static final String CREATE_NEW_GROUP = "http://coms-309-011.class.las.iastate.edu:8080/groups/new";
+    /**
+     * Url for the GET function which will return all members
+     */
     public static final String GET_MEMBERS = "http://coms-309-011.class.las.iastate.edu:8080/members";
+    /**
+     * Url for the POST function which will create a new member
+     */
     public static final String CREATE_NEW_MEMBER = "http://coms-309-011.class.las.iastate.edu:8080/members/new";
 }
