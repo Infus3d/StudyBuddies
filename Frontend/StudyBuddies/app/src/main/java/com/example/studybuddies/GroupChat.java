186dadc69bfda8b5dff98b8ada4157b5575e46f6
Andy <ajbruder@iastate.edu>
2022-05-02 22:01:15 -0500

    created chat bubble drawables and group chat activity

--- Frontend/StudyBuddies/app/src/main/java/com/example/studybuddies/GroupChat.java
+++ Frontend/StudyBuddies/app/src/main/java/com/example/studybuddies/GroupChat.java
@@ -2,13 +2,24 @@ package com.example.studybuddies;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class GroupChat extends AppCompatActivity {
+import com.example.studybuddies.databinding.ActivityGroupChatBinding;
+
+public class GroupChat extends DrawerBaseActivity {
+
+    ActivityGroupChatBinding activityGroupChatBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_group_chat);
+        activityGroupChatBinding = ActivityGroupChatBinding.inflate(getLayoutInflater());
+        setContentView(activityGroupChatBinding.getRoot());
+
+        Intent incoming = getIntent();
+        Bundle extras = incoming.getExtras();
+
+
     }
 }
