81313decfd3fffd2df9c4a05b9035141b2737830
Andy <ajbruder@iastate.edu>
2022-05-05 04:00:45 -0500

    updated chat page do display messages as relative layouts

--- Frontend/StudyBuddies/app/src/main/java/com/example/studybuddies/GroupChat.java
+++ Frontend/StudyBuddies/app/src/main/java/com/example/studybuddies/GroupChat.java
@@ -3,18 +3,23 @@ package com.example.studybuddies;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Message;
+import android.text.Layout;
+import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.studybuddies.databinding.ActivityGroupChatBinding;
@@ -28,6 +33,7 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 import org.java_websocket.client.WebSocketClient;
 
@@ -39,6 +45,8 @@ public class GroupChat extends DrawerBaseActivity {
     private String email_s;
     private String password_s;
     private String location_s;
+
+    private int messageCounter, textViewCounter;
 
     private WebSocketClient client;
 
@@ -74,26 +82,39 @@ public class GroupChat extends DrawerBaseActivity {
         enterMessage = findViewById(R.id.enter_message_field);
         sendMessage = findViewById(R.id.send_message_button);
 
+        messageCounter = 0; textViewCounter = 0;
+
         ChatMessage in = new ChatMessage(4, getTime(), "Omar Muhammetkulyyev", groupId, "hey, how are you?");
         ChatMessage out = new ChatMessage(id, getTime(), username_s, groupId, "i'm good, how are you?");
 
         ArrayList<ChatMessage> messages = new ArrayList<ChatMessage>();
         messages.add(in);
         messages.add(out);
-
+        messages.add(in);
+        messages.add(out);
+        messages.add(in);
+        messages.add(out);
+        messages.add(in);
+        messages.add(out);
+        messages.add(in);
+        messages.add(in);
+        messages.add(in);
+        messages.add(out);
         //openConnection();
 
         displayExistingMessages(messages);
 
     }
 
-    public void getExistingMessages(){
-
-    }
-
     private void displayExistingMessages(ArrayList<ChatMessage> messages) {
         for (ChatMessage message : messages) {
-            displayMessage(messageDisplay, message);
+            boolean outgoing = message.getUserId() == id;
+            if (outgoing) {
+                createOutgoingMessage(message);
+            } else {
+                createIncomingMessage(message);
+            }
+            messageCounter++;
         }
     }
 
@@ -102,18 +123,29 @@ public class GroupChat extends DrawerBaseActivity {
         return now.get(Calendar.HOUR_OF_DAY) + ":" + now.get(Calendar.MINUTE);
     }
 
-    private void displayMessage(LinearLayout messageHolder, ChatMessage message) {
-        LinearLayout layout = new LinearLayout(this);
-        layout.setOrientation(LinearLayout.HORIZONTAL);
-        Boolean outgoing = message.getUserId() == id;
-        if (outgoing) {
-            addMessage(outgoing, layout, message);
-            addProfile(layout, message);
-        } else {
-            addProfile(layout, message);
-            addMessage(outgoing, layout, message);
-        }
-        messageHolder.addView(layout);
+    public void createIncomingMessage(ChatMessage message) {
+        View messageRow = getLayoutInflater().inflate(R.layout.incoming_messge_row, messageDisplay, false);
+        messageRow.setId(messageCounter);
+        messageDisplay.addView(messageRow);
+        TextView profile = findViewById(R.id.profile_circle);
+        profile.setId(textViewCounter++);
+        profile.setText(message.getAuthor());
+        TextView messageBox = findViewById(R.id.message_box);
+        messageBox.setId(textViewCounter++);
+        messageBox.setText(message.getMessage());
+    }
+
+    private void createOutgoingMessage(ChatMessage message) {
+        View messageRow = getLayoutInflater().inflate(R.layout.outgoing_message_row, messageDisplay, false);
+        messageRow.setId(messageCounter);
+        messageDisplay.addView(messageRow);
+        TextView profile = findViewById(R.id.profile_circle);
+        profile.setId(textViewCounter++);
+        profile.setText(message.getAuthor());
+        TextView messageBox = findViewById(R.id.message_box);
+        messageBox.setId(textViewCounter++);
+        messageBox.setText(message.getMessage());
+        messageBox.setGravity(View.FOCUS_RIGHT);
     }
 
     /**
@@ -167,34 +199,6 @@ public class GroupChat extends DrawerBaseActivity {
             }
         });
 
-    }
-
-    public void addMessage(boolean outgoing, LinearLayout layout, ChatMessage message) {
-        TextView messageView = new TextView(this);
-        if (outgoing) {
-            messageView.setBackgroundResource(R.drawable.outgoing_message);
-            messageView.setGravity(View.FOCUS_RIGHT);
-        } else {
-            messageView.setBackgroundResource(R.drawable.incoming_message);
-        }
-        messageView.setText(message.getMessage());
-        messageView.setTextColor(Color.BLACK);
-        layout.addView(messageView);
-    }
-
-    private void addProfile(LinearLayout layout, ChatMessage message) {
-        String usernameDisplay;
-        TextView profileView = new TextView(this);
-        profileView.setBackgroundResource(R.drawable.profile_holder);
-        if (message.getAuthor().length() > 8) {
-            usernameDisplay = message.getAuthor().substring(0, 8) + "...";
-        } else {
-            usernameDisplay = message.getAuthor();
-        }
-        profileView.setText(usernameDisplay);
-        profileView.setTextColor(Color.BLACK);
-        profileView.setHeight(50);
-        layout.addView(profileView);
     }
 
 }
