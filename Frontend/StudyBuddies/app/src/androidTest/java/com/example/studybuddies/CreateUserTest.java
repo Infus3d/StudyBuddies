81ad1d7e4cf108e7d2bb49bb2e5472186426dd60
Omar Muhammetkulyyev <omar99@iastate.edu>
2022-05-06 18:09:50 -0500

    Added more tests

--- Frontend/StudyBuddies/app/src/androidTest/java/com/example/studybuddies/CreateUserTest.java
+++ Frontend/StudyBuddies/app/src/androidTest/java/com/example/studybuddies/CreateUserTest.java
@@ -20,12 +20,15 @@ import android.content.ComponentName;
 import android.view.Gravity;
 
 import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.NoMatchingViewException;
 import androidx.test.espresso.ViewAction;
 import androidx.test.espresso.contrib.DrawerActions;
 import androidx.test.espresso.contrib.NavigationViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
+
+import junit.framework.AssertionFailedError;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -39,8 +42,13 @@ public class CreateUserTest {
     public ActivityScenarioRule<CreateUser> activityRule = new ActivityScenarioRule<CreateUser>(CreateUser.class);
 
     @Test
-    public void validEmailTest(){
-        logOut();
+    public void invalidEmailTest(){
+        try{
+            onView(withId(R.id.create_button)).check(matches(isDisplayed()));
+        }
+        catch (NoMatchingViewException e) {
+            logOut();
+        }
         String username = "testuser1";
         String password1 = "password";
         String password2 = "password";
@@ -55,6 +63,57 @@ public class CreateUserTest {
         onView(withId(R.id.location)).perform(typeText(location), closeSoftKeyboard());
         onView(withId(R.id.create_button)).perform(click());
         onView(withId(R.id.error_display)).check(matches(withText(errorMessage)));
+
+        login();
+    }
+
+    @Test
+    public void validEmailTest(){
+        try{
+            onView(withId(R.id.create_button)).check(matches(isDisplayed()));
+        }
+        catch (NoMatchingViewException e) {
+            logOut();
+        }
+        String username = "testuser7";
+        String password1 = "password";
+        String password2 = "password";
+        String email = "testuser7@email.com";
+        String location = "City, State";
+        String errorMessage = "invalid email address";
+
+        onView(withId(R.id.user_name)).perform(typeText(username), closeSoftKeyboard());
+        onView(withId(R.id.password_one)).perform(typeText(password1), closeSoftKeyboard());
+        onView(withId(R.id.password_two)).perform(typeText(password2), closeSoftKeyboard());
+        onView(withId(R.id.email_address)).perform(typeText(email), closeSoftKeyboard());
+        onView(withId(R.id.location)).perform(typeText(location), closeSoftKeyboard());
+        onView(withId(R.id.create_button)).perform(click());
+
+        try {
+            Thread.sleep(SIMULATED_DELAY_MS);
+        } catch (InterruptedException e) {
+        }
+
+        onView(withId(R.id.create_group)).check(matches(isDisplayed()));
+    }
+
+    void login(){
+        onView(withId(R.id.login)).perform(click());
+        String username = "testusername";
+        String password = "password";
+        String errorMessage = "";
+
+        onView(withId(R.id.enter_user_name)).perform(typeText(username));
+        onView(withId(R.id.enter_password)).perform(typeText(password));
+        onView(withId(R.id.login_button)).perform(click());
+//        intended(hasComponent(new ComponentName(getTargetContext(), ExpectedActivity.class)));
+
+
+        try {
+            Thread.sleep(SIMULATED_DELAY_MS);
+        } catch (InterruptedException e) {
+        }
+        onView(withId(R.id.create_group)).check(matches(isDisplayed()));
     }
 
     void logOut(){
